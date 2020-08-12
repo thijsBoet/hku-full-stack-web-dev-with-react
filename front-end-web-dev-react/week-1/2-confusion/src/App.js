23aae0a93c2f9098632880fd9c46789ded253902
Matthijs Boet <m.boet@chello.nl>
2020-08-12 20:54:24 +0200

    scaffoldign confusion

--- front-end-web-dev-react/week-1/2-confusion/src/App.js
+++ front-end-web-dev-react/week-1/2-confusion/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <img className="App-logo" src={logo} alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+          <p className="App-intro">
+            To get started, edit{" "}
+            <code>
+              <a className="App-link" href="./App.js">
+                src/App.js
+              </a>
+            </code>{" "}
+            and save.
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
